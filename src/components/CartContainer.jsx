1aa49aabe085ba9ca0ba819baa92bf029943bf62
Sergio González Herrera <109561989+SergioGonzalezHerrera@users.noreply.github.com>
2023-03-14 12:39:32 -0300

    Correcciones al carrito

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -1,30 +1,63 @@
 import { useCartContext } from "../context/CartContext"
+import { Link } from "react-router-dom";
 
 const CartContainer = () => {
-  
+
   const { cartList, vaciarCarrito, precioTotal, eliminarProducto } = useCartContext()
 
   return (
     <div>
-      <div>
-        {
-          cartList.map(prodCart => (
-              <label key={prodCart.id}>
-                <img src={prodCart.foto} className='w-25' />
-                Nombre: {prodCart.name}
-                Cantidad: {prodCart.cantidad}
-                Valor unitario: {prodCart.price}  
-                <div>Valor total: {precioTotal()}</div>
-                <button variant="danger" onClick={ () =>eliminarProducto(prodCart.id)}>Eliminar del carrito</button>
-              </label>
-              
-          ))
-        }
-        
+      {cartList.length > 0 ? (
+        <div>
+          <div>
+            {
+              cartList.map(prodCart => (
+                <label key={prodCart.id}>
+                  <img src={prodCart.foto} className='w-25' />
+                  Nombre: {prodCart.name}
+                  Cantidad: {prodCart.cantidad}
+                  Valor unitario: {prodCart.price}
+                  <div>Valor total: {precioTotal()}</div>
+                  <button variant="danger" onClick={() => eliminarProducto(prodCart.id)}>Eliminar del carrito</button>
+                </label>
+              ))
+            }
+          </div>
+          <button onClick={vaciarCarrito}>Vaciar carrito</button>
         </div>
-        <button onClick={vaciarCarrito}>Vaciar carrito</button>
+      ) : (
+        // Si el array está vacío, mostramos estos elementos
+        <div>
+          <h1>Aún no has agregado productos a tu carrito!</h1>
+          
+          <Link to='/'><button > Volver al inicio</button></Link>
+        </div>
+      )}
     </div>
-  )
+  );
 }
+
+// return (
+//   <div>
+//     <div>
+//       {
+//         cartList.map(prodCart => (
+//           <label key={prodCart.id}>
+//             <img src={prodCart.foto} className='w-25' />
+//             Nombre: {prodCart.name}
+//             Cantidad: {prodCart.cantidad}
+//             Valor unitario: {prodCart.price}
+//             <div>Valor total: {precioTotal()}</div>
+//             <button variant="danger" onClick={() => eliminarProducto(prodCart.id)}>Eliminar del carrito</button>
+//           </label>
+
+//         ))
+//       }
+
+//     </div>
+//     <button onClick={vaciarCarrito}>Vaciar carrito</button>
+//   </div>
+// )
+
 
 export default CartContainer
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,44 +1,44 @@
 import { createContext, useContext, useState } from "react";
 
-const CartContext = createContext ([]);
+const CartContext = createContext([]);
 
 export const useCartContext = () => {
     return useContext(CartContext)
 }
 
-export const CartContextProvider = ({children}) => {
+export const CartContextProvider = ({ children }) => {
 
-    const [cartList, setCartList]= useState([])
+    const [cartList, setCartList] = useState([])
 
     const agregarCart = (producto) => {
-        const indexProd = cartList.findIndex(product => product.id === product.id)
-        if(indexProd!==-1){
+        const indexProd = cartList.findIndex(product => product.id === producto.id)
+        if (indexProd !== -1) {
             cartList[indexProd].cantidad += producto.cantidad
             setCartList([...cartList])
             return
         }
-        setCartList( [ ...cartList, producto ] )
+        setCartList([...cartList, producto])
     }
 
-    const precioTotal = () => cartList.reduce( (count, producto) => count += (producto.cantidad*producto.price), 0)
-    
-    const cantidadTotal = () => cartList.reduce( (count, producto) => count += producto.cantidad, 0)
+    const precioTotal = () => cartList.reduce((count, producto) => count += (producto.cantidad * producto.price), 0)
+
+    const cantidadTotal = () => cartList.reduce((count, producto) => count += producto.cantidad, 0)
 
     const eliminarProducto = (id) => setCartList(cartList.filter(prod => prod.id !== id))
 
-    const vaciarCarrito = () =>{
+    const vaciarCarrito = () => {
         setCartList([])
     }
 
     return (
-        <CartContext.Provider value = {{
+        <CartContext.Provider value={{
             cartList,
             agregarCart,
             vaciarCarrito,
             precioTotal,
             cantidadTotal,
             eliminarProducto
-            }}>
+        }}>
             {children}
         </CartContext.Provider>
     )
